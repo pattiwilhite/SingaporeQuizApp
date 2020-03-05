caf05039007bec129302587f04611ebf6ff49a15
pattiwilhite <56936033+pattiwilhite@users.noreply.github.com>
2020-03-05 13:15:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,123 @@
+const STORE = [
+    {
+      question: 'What can you not buy in Singapore?',
+      answers: [
+        'Alcohol',
+        'Diet Coke',
+        'Chewing Gum',
+        'Exotic Pets'
+        ],
+      correctAnswer: 'Chewing Gum',
+      icon: 'https://i.imgur.com/Rue4wCj.png',
+      alt: 'tent icon'
+    },
+    {
+      question: 'What is the symbol of Singapore?',
+      answers: [
+        'Monkey',
+        'Lion',
+        'Dragon',
+        'Cat'
+        ],
+        correctAnswer: 'Lion',
+        icon: 'https://i.imgur.com/iQ0FBhB.png',
+        alt: 'shield for 10 essentials icon'
+    },
+    {
+      question: 'Which of these was founded in Singapore?',
+      answers: [
+        'World Health Organization',
+        'The Roundabout Society',
+        'The Dill Pickle Club',
+        'World Toilet Organization'
+        ],
+        correctAnswer: 'World Toilet Organization',
+        icon: 'https://i.imgur.com/uXukVjk.png',
+        alt: 'weight icon'
+    },
+    {
+      question: 'What is the currecny of Singapore?',
+      answers: [
+        'Dollar',
+        'Baht',
+        'Rupee',
+        'Euro'
+        ],
+      correctAnswer: 'Dollar',
+      icon: 'https://i.imgur.com/i0Q515j.png',
+      alt: 'bear paw icon'
+    },
+    {
+      question: 'What is the maximum height of skyscraper allowed in Singapore?',
+      answers: [
+        '280 meters',
+        '120 meters',
+        '300 meters',
+        '380 meters'
+        ],
+      correctAnswer: '280 meters',
+      icon: 'https://i.imgur.com/LHRMcBd.png',
+      alt: 'hat icon'
+    },
+    {
+      question: 'What is the name of the International Airport in Singapore?',
+      answers: [
+        'Suvarnabhumi Airport',
+        'Heathrow Airport',
+        'Changi International Airport',
+        'LaGuardia Airport'
+        ],
+      correctAnswer: 'Changi International Airport',
+      icon: 'https://i.imgur.com/XEwf3tK.png',
+      alt: 'water bottle icon'
+    },
+    {
+      question: 'What is the famous street that runs through the commercial district of Singapore?',
+      answers: [
+      'Sukhumvit Road',
+      'Orchard Road',
+      'Orange Street',
+      'Hanshin Expressway'
+      ],
+      correctAnswer: 'Orchard Road',
+      icon: 'https://i.imgur.com/Kk9t3qL.png',
+      alt: 'stream icon'
+    },
+    {
+      question: 'The most popular tourist attraction in Singapore is and Island called what?',
+      answers: [
+        'Sentosa Island',
+        'Bintan Island',
+        'Batam Island',
+        'Staten Island'
+      ],
+      correctAnswer: 'Sentosa Island',
+      icon: 'https://i.imgur.com/A2cWS88.png',
+      alt: 'compass icon'
+    },
+    {
+      question: 'The name Singapore is Derived from the work Sanskrit. What does it mean?',
+      answers: [
+      'Cheap Karaoke',
+      'Beautiful Harbor',
+      'Tiger City',
+      'Lion City'
+      ],
+      correctAnswer: 'Lion City',
+      icon: 'https://i.imgur.com/VwFRlXL.png',
+      alt: 'bear head icon'
+    },
+    {
+      question: 'What nickname has the city been unofficially been referred to since the 1980s?',
+      answers: [
+        'The Clay City',
+        'The Jungle City',
+        'The Fine City',
+        'The Sin City'
+    ],
+    correctAnswer: 'The Fine City',
+    icon: 'https://i.imgur.com/YdcTVPW.png',
+    alt: 'skull and crossbones icon'
+    }
+];
 
