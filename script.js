976a5a52a2c05a933fb41aeb2b7ab3e3fb32c62c
pattiwilhite <56936033+pattiwilhite@users.noreply.github.com>
2020-03-05 15:16:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const STORE = [
         'Exotic Pets'
         ],
       correctAnswer: 'Chewing Gum',
-      icon: 'CorrectCheck.png',
+      icon: 'media/CorrectCheck.png',
       alt: 'Check icon'
     },
     {
